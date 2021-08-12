a66e240140121bbdd7e673a0ed8fefa6fae9b60a
derek <derekle.creative@gmail.com>
2021-08-11 21:19:27 -0500

    commit tree 3b91de09fa1235aa2e055fbdd8727480564b8282

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(r){
+    console.log(r[1].result);
+    const found = r.find( ({ result }) => result === 'W' );
+    console.log(found)
+    if (found){
+        return found.year;
+    } else return found;
+}
